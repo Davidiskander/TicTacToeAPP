2f33d3fe52e08f9825e3f9c1f69446784e4ad82a
David Iskander <david.iskander.di@gmail.com>
2016-03-17 15:52:40 -0700

    again

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -10,6 +10,21 @@
 
 @interface ViewController ()
 
+//1st coulmn of buttons
+@property (weak, nonatomic) IBOutlet UIButton *buttonA0;
+@property (weak, nonatomic) IBOutlet UIButton *buttonA1;
+@property (weak, nonatomic) IBOutlet UIButton *buttonA2;
+
+//2nd coulmn of buttons
+@property (weak, nonatomic) IBOutlet UIButton *buttonB0;
+@property (weak, nonatomic) IBOutlet UIButton *buttonB1;
+@property (weak, nonatomic) IBOutlet UIButton *buttonB2;
+
+//3rd coulmn of buttons
+@property (weak, nonatomic) IBOutlet UIButton *buttonC0;
+@property (weak, nonatomic) IBOutlet UIButton *buttonC1;
+@property (weak, nonatomic) IBOutlet UIButton *buttonC2;
+
 @end
 
 @implementation ViewController
@@ -19,9 +34,7 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
+
 
 @end
