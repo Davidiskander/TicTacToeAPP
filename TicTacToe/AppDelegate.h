a639a018f0289bb66ccf6c0693be3884bac88a5e
David Iskander <david.iskander.di@gmail.com>
2016-03-17 15:19:04 -0700

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by David Iskander on 3/17/16.
+//  Copyright © 2016 DIskander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by David Iskander on 3/17/16.
+//  Copyright © 2016 DIskander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
