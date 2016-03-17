ff7a7e22ad3f5617423a5b4a7022ccf328aadb18
Chris Serra <csserra@gmail.com@gmail.com>
2016-03-17 16:35:44 -0700

    early methods

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -25,13 +25,59 @@
 @property (weak, nonatomic) IBOutlet UIButton *buttonC1;
 @property (weak, nonatomic) IBOutlet UIButton *buttonC2;
 
+
+@property NSArray *tiles;
+@property BOOL isXTurn;
+
 @end
 
 @implementation ViewController
 
+- (IBAction)onButtonPress:(UIButton *)sender {
+}
+
+
+- (void)setButtonLabel:(UIButton *)button {
+
+    // set label
+    NSString *title;
+    if (self.isXTurn) {
+        title = @"X";
+    } else {
+        title = @"O";
+    }
+    [button setTitle:title forState:UIControlStateNormal];
+
+    // disable button
+    button.enabled = NO;
+}
+
+- (void)changeTurn {
+    self.isXTurn = ! self.isXTurn;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.isXTurn = YES;
+    self.tiles =
+    @[
+      @[self.buttonA0, self.buttonA1, self.buttonA2],
+      @[self.buttonB0, self.buttonB1, self.buttonB2],
+      @[self.buttonC0, self.buttonC1, self.buttonC2]
+      ];
+    
+    for (int x = 0; x < 3; x++) {
+        for (int y = 0; y < 3; y++) {
+            UIButton *button = self.tiles[x][y];
+
+            
+            NSString *myName = [NSString stringWithFormat:@"%i, %i", x, y];
+            [button setTitle:myName forState:UIControlStateNormal];
+            //NSLog([NSString stringWithFormat:@"%i, %i", x, y]);
+        }
+    }
+   
 }
 
 
