768965b7ce24f6ed908ebf7158ce202464843b52
Chris Serra <csserra@gmail.com@gmail.com>
2016-03-17 19:58:31 -0700

    add isGameOver()

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -70,8 +70,11 @@
 // User playing
 - (IBAction)onButtonPress:(UIButton *)sender {
     NSLog(@"Button pressed: %@", [sender currentTitle]);
-    [self setButtonLabel:sender];
+    [self setButtonTitle:sender];
     //isGameOver
+    if ([self isGameOver]) {
+        NSLog(@"GAME OVER");
+    }
     //winner = sender
     [self changeTurn];
 }
@@ -84,14 +87,6 @@
         return @"O";
     }
 }
-
-- (void)setButtonLabel:(UIButton *)button {
-    // set label
-    [button setTitle:[self playerMark] forState:UIControlStateNormal];
-    // disable button
-    button.enabled = NO;
-}
-
 
 - (void)changeTurn {
     self.isXTurn = ! self.isXTurn;
@@ -117,6 +112,126 @@
 }
 
             
+- (NSString *)getButtonTitle:(UIButton *)button {
+    return [button titleForState:[button state]];
+}
+- (void)setButtonTitle:(UIButton *)button {
+    
+    // set label
+    [button setTitle:[self playerMark] forState:UIControlStateNormal];
+    [button setTitle:[self playerMark] forState:UIControlStateDisabled];
+    
+    // disable button
+    button.enabled = NO;
+}
+
+
+#pragma mark -
+#pragma mark isGameOver
+
+
+- (BOOL)isGameOver {
+    int n = self.tiles.count;
+    
+    // check each row
+    for (int i = 0; i < n; i++) {
+        if ([self isWinningRow:i]) { return YES; }
+    }
+    
+    // check each column
+    for (int i = 0; i < n; i++) {
+        if ([self isWinningColumn:i]) { return YES; }
+    }
+    
+    // check diagonals
+    if ([self isWinningDiagSoutheast]) { return YES; }
+    if ([self isWinningDiagNortheast]) { return YES; }
+    
+    // no winning scenarios
+    return NO;
+}
+
+- (BOOL)isWinningRow:(int)y {
+    UIButton *firstButton = self.tiles[0][y];
+    NSString *first = [self getButtonTitle:firstButton];
+    if (! first) { return NO; }
+    
+    int n = self.tiles.count;
+    for (int x = 1; x < n; x++) {
+        UIButton *otherButton = self.tiles[x][y];
+        NSString *other = [self getButtonTitle:otherButton];
+        if ([other isEqualToString:first]) {
+            // match: keep checking
+        } else {
+            // different: not a winner
+            return NO;
+        }
+    }
+    // all tiles matched: winner
+    return YES;
+}
+
+- (BOOL)isWinningColumn:(int)x {
+    UIButton *firstButton = self.tiles[x][0];
+    NSString *first = [self getButtonTitle:firstButton];
+    if (! first) { return NO; }
+    
+    int n = self.tiles.count;
+    for (int y = 1; y < n; y++) {
+        UIButton *otherButton = self.tiles[x][y];
+        NSString *other = [self getButtonTitle:otherButton];
+        if ([other isEqualToString:first]) {
+            // match: keep checking
+        } else {
+            // different: not a winner
+            return NO;
+        }
+    }
+    // all tiles matched: winner
+    return YES;
+}
+
+// diagonal #1: (0,0) to (2,2)
+- (BOOL)isWinningDiagSoutheast {
+    UIButton *firstButton = self.tiles[0][0];
+    NSString *first = [self getButtonTitle:firstButton];
+    if (! first) { return NO; }
+    
+    int n = self.tiles.count;
+    for (int xy = 1; xy < n; xy++) {
+        UIButton *otherButton = self.tiles[xy][xy];
+        NSString *other = [self getButtonTitle:otherButton];
+        if ([other isEqualToString:first]) {
+            // match: keep checking
+        } else {
+            // different: not a winner
+            return NO;
+        }
+    }
+    // all tiles matched: winner
+    return YES;
+}
+
+// diagonal #1: (0,0) to (2,2)
+- (BOOL)isWinningDiagNortheast {
+    int n = self.tiles.count;
+    UIButton *firstButton = self.tiles[n-1][0];
+    NSString *first = [self getButtonTitle:firstButton];
+    if (! first) { return NO; }
+    
+    for (int xy = 1; xy < n; xy++) {
+        UIButton *otherButton = self.tiles[xy][n-1-xy];
+        NSString *other = [self getButtonTitle:otherButton];
+        if ([other isEqualToString:first]) {
+            // match: keep checking
+        } else {
+            // different: not a winner
+            return NO;
+        }
+    }
+    // all tiles matched: winner
+    return YES;
+}
 
 
 @end
